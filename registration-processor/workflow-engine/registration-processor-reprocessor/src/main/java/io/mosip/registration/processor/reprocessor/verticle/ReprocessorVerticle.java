a6c8cd36a76cc89a302c4c96c254ebe90c2705ed
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2025-10-09 15:43:10 +0530

    Reprocessor Performance Fix
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>

--- registration-processor/workflow-engine/registration-processor-reprocessor/src/main/java/io/mosip/registration/processor/reprocessor/verticle/ReprocessorVerticle.java
+++ registration-processor/workflow-engine/registration-processor-reprocessor/src/main/java/io/mosip/registration/processor/reprocessor/verticle/ReprocessorVerticle.java
@@ -550,6 +550,9 @@ public class ReprocessorVerticle extends MosipVerticleAPIManager {
 					// Fetch unprocessed packets
 
 					List<String> skipRegIdList = cacheList != null && !cacheList.isEmpty() ? cacheList.stream().map(e -> e.getRegistrationId()).collect(Collectors.toList()) : Collections.emptyList();
+
+					if(skipRegIdList.isEmpty()) skipRegIdList.add("");
+
 					return reprocessorVerticalService.fetchUnProcessedPackets(processList, recordFetchCount, elapseTime,
 							reprocessCount, (!statusValList.isEmpty() ? statusValList : statusList), reprocessExcludeStageNames, skipRegIdList)
 							.thenAccept(result -> {
